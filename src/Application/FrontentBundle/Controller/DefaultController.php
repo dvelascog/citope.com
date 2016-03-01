c1cb04010482255f121822461603490e119c309d
David Velasco <fodaveg@gmail.com>
2016-03-01 22:37:51 +0100

    dfdsdsdfdsaafddsafw

--- src/Application/FrontentBundle/Controller/DefaultController.php
+++ src/Application/FrontentBundle/Controller/DefaultController.php
@@ -5,7 +5,9 @@ namespace Application\FrontentBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-
+use Google_Client;
+use Google_Service_Customsearch_ResultImage;
+use Google_Service_Customsearch;
 class DefaultController extends Controller
 {
     /**
@@ -14,6 +16,25 @@ class DefaultController extends Controller
      */
     public function homeAction()
     {
+
+        $client = new Google_Client();
+        $client->setApplicationName("citope.com");
+        $client->setDeveloperKey("AIzaSyAobgKimEctoeGN1esZ-W_enHX-Y_VOISQ");
+
+        $service = new Google_Service_Customsearch($client);
+        $optParams = array();
+        $results = $service->cse->listCse('boobs', $optParams);
+        var_dump($service);
+        die;
+
+        foreach ($results as $item) {
+            echo $item['volumeInfo']['title'], "<br /> \n";
+        }
+
+
+die;
+
+
         $section = 'home';
 
         return array('section' => $section);
