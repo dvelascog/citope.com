12ae566c4a410979b2fcd58bf4258000a6ef3611
David Velasco <dvelasco@wearemarketing.com>
2016-03-07 09:27:27 +0100

    WIP ASAP

--- src/Application/FrontentBundle/Utils/MediaProcessor.php
+++ src/Application/FrontentBundle/Utils/MediaProcessor.php
@@ -6,7 +6,6 @@ use Doctrine\ORM\EntityManager;
 use Liip\ImagineBundle\Controller\ImagineController;
 use Symfony\Component\HttpFoundation\Request;
 
-
 /**
  * @author David Velasco <dvelasco@wearemarketing.com>
  */
@@ -29,30 +28,10 @@ class MediaProcessor
     public function ProcessImage($image, $width, $height)
     {
         $category = 'boobs';
-        $candidateImages = $this->em->getRepository(Media::class)->getAccurateImage($category,$width,$height);
-
-
+        $candidateImages = $this->em->getRepository(Media::class)->getAccurateImage($category, $width, $height);
 
         /** @var Media $selectedImage */
         $selectedImage = $candidateImages[array_rand($candidateImages, 1)];
-$baseUrl = $this->request->getSchemeAndHttpHost();
-//        die;
-//dump($selectedImage);
-//        die;
-        $imageUrl1 = $this->request->getUriForPath('/uploads/media/'.$selectedImage->getContext().'/0001/01/'.$selectedImage->getProviderReference());
-
-$imageUrl =    str_replace($baseUrl, '', $imageUrl1 );
-
-//        die($this->request->getCurrentRequest()->getRelativeUriForPath('/uploads/media/'.$selectedImage->getContext().'/0001/01/'.$selectedImage->getProviderReference(), false));
-                $imagemanagerResponse = $this->liipImagineController
-            ->filterAction(
-                $this->request,         // http request
-                $imageUrl,      // original image you want to apply a filter to
-                'q75'              // filter defined in config.yml
-            );
-
-
-
 
         return $selectedImage;
     }
@@ -60,18 +39,12 @@ $imageUrl =    str_replace($baseUrl, '', $imageUrl1 );
 
     public function ServeImages($all = false, $category = null)
     {
-
-        if(!$all && $category)
-        {
+        if (!$all && $category) {
             return $images = $this->em->getRepository(Media::class)->findBy(
                 array('context' => $category)
             );
-
-        }
-        else
-        {
+        } else {
             return $images = $this->em->getRepository(Media::class)->findAll();
         }
     }
-
 }
